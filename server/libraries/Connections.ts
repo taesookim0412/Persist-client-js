2408c3e78678976775b67ee2ee156747de2cbc08
Tae Soo Kim <taesoo.kim0412@gmail.com>
2021-07-28 15:41:58 -0700

    0728 - cross platform (java client + nodejs client)

--- server/libraries/Connections.ts
+++ server/libraries/Connections.ts
@@ -1,21 +1,35 @@
 import http = require('http');
-const agent = new http.Agent({keepAlive: true})
 
 export class Connections{
     constructor() {
         this.listenToServer("localhost", "8090", "/socket/persist")
     }
+    //https://stackoverflow.com/questions/23874954/is-there-a-default-timeout-in-node-js-for-http-request
     listenToServer = async (host: string, port: string, path: string) => {
         const req = http.request({
                 host: host,
                 port: port,
                 path: path,
                 method: 'GET',
-                agent: agent,
+                // agent: agent,
                 headers: {'Content-Type': 'application/json'}
             }, (res) => {
                 res.on('data', (chunk) => {
-                    console.log(chunk.toString())
+                    console.log(Date.now())
+                    try{
+                        console.log(chunk.toString());
+                    }
+                    catch (e) {
+                        console.log("tostring failed")
+                    }
+                    try{
+                        console.log(JSON.parse(chunk))
+                    }
+                    catch(e) {
+                        console.log(e);
+                    }
+                    // console.log(chunk)
+                    // console.log(JSON.parse(chunk));
                     // const data = JSON.parse(chunk)
                     // const message = data['message']
                     // if (message === 'new job'){
@@ -33,12 +47,16 @@ export class Connections{
             }
         )
         req.on('end', () => {
+            console.log('ended')
             req.end()
         })
         req.on('error', (err) => {
+            console.log('error')
+
             req.end()
         })
         req.end()
+        console.log('requested')
         // this.connections[Date.now().toString()] = req
     }
 }
